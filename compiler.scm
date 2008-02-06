cec2678636bfedc65796a0bc80ae11f3c446bb3d
Kragen Javier Sitaker <kragen@pobox.com>
2008-02-05 22:26:11 -0200

    renamed string-concatenate to string-append
    darcs-hash:20080206002611-c0035-ee569c4a410937d586b3b5462faf20c8d628572c

--- compiler.scm
+++ compiler.scm
@@ -149,23 +149,23 @@
 (define caddr (lambda (val) (cadr (cdr val))))
 
 ;; string manipulation (part of Basic Lisp Stuff)
-(define string-concatenate-3
+(define string-append-3
   (lambda (length s2 buf idx)
     (if (= idx (string-length buf)) buf
         (begin
           (string-set! buf idx (string-ref s2 (- idx length)))
-          (string-concatenate-3 length s2 buf (+ idx 1))))))
-(define string-concatenate-2
+          (string-append-3 length s2 buf (+ idx 1))))))
+(define string-append-2
   (lambda (s1 s2 buf idx)
     (if (= idx (string-length s1)) 
-        (string-concatenate-3 (string-length s1) s2 buf idx)
+        (string-append-3 (string-length s1) s2 buf idx)
         (begin
           (string-set! buf idx (string-ref s1 idx))
-          (string-concatenate-2 s1 s2 buf (+ idx 1))))))
-(define string-concatenate              ; string-append
+          (string-append-2 s1 s2 buf (+ idx 1))))))
+(define string-append          ; identical to standard "string-append"
   (lambda (s1 s2)
-    (string-concatenate-2 s1 s2 (make-string (+ (string-length s1) 
-                                                (string-length s2)))
+    (string-append-2 s1 s2 (make-string (+ (string-length s1) 
+                                           (string-length s2)))
                           0)))
 (define string-of-char-2
   (lambda (buf char) (begin (string-set! buf 0 char) buf)))
@@ -178,8 +178,8 @@
 (define number-to-string-2
   (lambda (num)
     (if (= num 0) ""
-        (string-concatenate (number-to-string-2 (quotient num 10))
-                            (string-digit (remainder num 10))))))
+        (string-append (number-to-string-2 (quotient num 10))
+                       (string-digit (remainder num 10))))))
 (define number-to-string                ; number->string
   (lambda (num) (if (= num 0) "0" (number-to-string-2 num))))
 
@@ -302,7 +302,7 @@
 ;; Represent a string appropriately for the output assembly language file.
 (define asm-represent-string
   (lambda (string)
-    (string-concatenate "\"" (string-concatenate (backslashify string) "\""))))
+    (string-append "\"" (string-append (backslashify string) "\""))))
 
 (define ascii (lambda (string) (insn ".ascii " (asm-represent-string string))))
 
