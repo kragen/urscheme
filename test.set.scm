0ae97fd492620a363e8a54a84385dbbf6bb0cf2c
Kragen Javier Sitaker <kragen@pobox.com>
2008-02-20 22:18:48 -0200

    adding test for setting global variable setting. oops!
    darcs-hash:20080221001848-c0035-dd40dc996333b1f4926bf4b138e5303643d50a98

---
+++ test.set.scm
@@ -0,0 +1,32 @@
+;;; Tests of set!
+;; There are three ways variables are stored at the moment: globally,
+;; on the stack, and in the heap.
+
+(define global "global")
+(define (printglobal) (display global) (newline))
+(printglobal)
+(set! global "global variable")
+(printglobal)
+(set! global "global variable gets set!")
+(printglobal)
+
+; (define (printlocal local)
+;   (set! local "local variable always the same")
+;   (display local)
+;   (newline))
+; (printlocal "a")
+; (printlocal "b")
+
+; (define heap-printer
+;   (let ((val "original heap var"))
+;     (lambda (cmd arg)
+;       (case cmd
+;         ((set!) (set! val arg))
+;         ((print) (display val) (newline))
+;         (else (error "bad cmd" cmd))))))
+
+; (heap-printer 'print 0)
+; (heap-printer 'set! "heap var")
+; (heap-printer 'print 0)
+; (heap-printer 'set! "heap var changes!")
+; (heap-printer 'print 0)
